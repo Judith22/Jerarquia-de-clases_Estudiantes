bf5fb2e925d33fcfa83dc15c774673616dbfd19a
Judith22 <judith_220291@hotmail.com>
2017-03-09 16:18:24 -0600

    Jerarquia de clases_Estudiantes

---
+++ Estudiantes/src/isc/isma4/estudiantes/Administracion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isc.isma4.estudiantes;
+
+/**
+ *
+ * @author lenovo
+ */
+ public class Administracion extends Estudiante{
+    String color;
+    public Administracion(String color){
+        this.color = color;
+    }
+    
+    
+    @Override
+    public String getColor(){
+        return color;
+    }
+}   
+
---
+++ Estudiantes/src/isc/isma4/estudiantes/Gastronomia.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isc.isma4.estudiantes;
+
+/**
+ *
+ * @author lenovo
+ */
+ public class Gastronomia extends Estudiante{
+    String color;
+    public Gastronomia(String color){
+        this.color = color;
+    }
+    
+    
+    @Override
+    public String getColor(){
+        return color;
+    }
+}
